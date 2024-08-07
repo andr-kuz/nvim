bd571be1f147511356316d914b7b90f37d6ae6bb
Andrey <awakum192@gmail.com>
2024-08-07 19:03:07 +0300

    Explore remap

--- lua/valtrois/remap.lua
+++ lua/valtrois/remap.lua
@@ -8,3 +8,5 @@ vim.keymap.set({'n', 'x'}, '<leader>d', 'd')
 
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+vim.keymap.set('n', '<leader>e', '<Cmd>Ex<CR>') 
