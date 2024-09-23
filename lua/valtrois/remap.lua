f8be68062ba348715d84407221ec8f545fdc3a66
Andrey <awakum192@gmail.com>
2024-09-23 07:23:34 +0300

    Build: wipe buffer when it's being closed and another shortcut

--- lua/valtrois/remap.lua
+++ lua/valtrois/remap.lua
@@ -10,5 +10,6 @@ vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = tr
 vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 vim.keymap.set('n', '<leader>e', '<Cmd>Ex<CR>')
-vim.keymap.set('n', '<leader>E', '<Cmd>vsplit ' .. vim.fs.dirname(vim.fn.expand('$MYVIMRC')) .. '/lua/$USER/remap.lua<CR>')
+vim.keymap.set('n', '<leader>E', '<Cmd>vsplit ' .. vim.fs.dirname(vim.fn.expand('$MYVIMRC')) .. '/lua/$USER/remap.lua<CR><Cmd>setlocal bufhidden=wipe<CR>')
+vim.keymap.set('n', '<leader>V', '<Cmd>vsplit ~/Projects/zettelkasten/Vim.md<CR><Cmd>setlocal bufhidden=wipe<CR>')
 vim.keymap.set('i', 'kj', '<Esc>')
