67cb0a45e41b25086546fc4552576d4fe66470aa
Andrey <awakum192@gmail.com>
2024-11-27 14:59:40 +0300

    --allow-empty

--- lua/valtrois/lazy/debugger.lua
+++ lua/valtrois/lazy/debugger.lua
@@ -24,13 +24,14 @@ return {
       local dap = require('dap')
       local dapui = require('dapui')
       dapui.setup()
-      dap.listeners.after.event_initialized['dapui_config'] = function ()
+      dapui.close('breakpoints')
+      dap.listeners.after.event_initialized.dapui_config = function ()
         dapui.open()
       end
-      dap.listeners.before.event_terminated['dapui_config'] = function ()
+      dap.listeners.before.event_terminated.dapui_config = function ()
         dapui.close()
       end
-      dap.listeners.before.event_exited['dapui_config'] = function ()
+      dap.listeners.before.event_exited.dapui_config = function ()
         dapui.close()
       end
     end,
