a7f95bf3cf74da57687a633d6128231e1bd959af
Andrey <awakum192@gmail.com>
2024-11-27 15:22:52 +0300

    --allow-empty

--- lua/valtrois/plugin_remap.lua
+++ lua/valtrois/plugin_remap.lua
@@ -1,9 +1,6 @@
 local dap = require('dap')
 vim.keymap.set('n', '<leader>db', dap.toggle_breakpoint)
-
--- local dappyton = require('dap-python')
--- vim.keymap.set('n', '<leader>dpr', dappyton.test_method)
-vim.keymap.set('n', '<leader>dr', dap.continue)
+vim.keymap.set('n', '<leader>dc', dap.continue)
 vim.keymap.set('n', '<leader>di', dap.step_into)
 vim.keymap.set('n', '<leader>do', dap.step_out)
 vim.keymap.set('n', '<leader>dn', dap.step_over)
